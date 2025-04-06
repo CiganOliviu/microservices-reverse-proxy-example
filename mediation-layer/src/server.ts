d75424718caf8f5ead80551906da57b79387ef85
CiganOliviuDavid <ciganoliviudavid@gmail.com>
2025-04-06 12:48:37 +0300

    Update mediation-layer

--- mediation-layer/src/server.ts
+++ mediation-layer/src/server.ts
@@ -14,5 +14,5 @@ app.use('/api', routes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Mediation Layer running on port ${PORT}`);
+    console.log(`Mediation Layer running on port ${PORT} - get data on route /api/data`);
 });
